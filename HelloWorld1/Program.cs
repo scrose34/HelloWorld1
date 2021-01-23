e3552b572204bb1891f11de92cbd3de19fb8fef5
Spencer <scrose34@gmail.com>
2021-01-22 21:29:39 -0500

    Version 2 HelloWorld

--- HelloWorld1/Program.cs
+++ HelloWorld1/Program.cs
@@ -6,7 +6,11 @@ namespace HelloWorld1
     {
         static void Main(string[] args)
         {
+            // Pause to display HelloWorld!
+            Console.WriteLine("Press any key to continue..");
             Console.WriteLine("Hello World!");
+            Console.ReadKey(true);
+
         }
     }
 }
